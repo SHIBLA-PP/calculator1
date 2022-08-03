91b997f633c7a9515a38191a56fd07a7b0497ce7
SHIBLA_PP <ppshibla@gmail.com>
2022-08-03 11:59:29 +0530

    calculator

---
+++ calc.js
@@ -0,0 +1,24 @@
+
+
+// 1. Display all numbers and operators other than AC and = inside input with id=result
+
+function display(num)
+{
+    result.value+=num
+}
+// 2. clear all content
+
+function clearAll()
+{
+    result.value=""
+}
+// 3. evaluate expression
+function output()
+{
+    result.value=eval(result.value)
+}
+// 4. remove last element
+function remove()
+{
+    result.value=(result.value).slice(0,-1)
+}
